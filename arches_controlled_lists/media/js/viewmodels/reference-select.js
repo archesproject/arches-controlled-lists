08e0aca2bce010acd887d65b7600ba67199daab2
Aaron Gundel <aaron.gundel@gmail.com>
2025-04-02 15:35:19 -0600

    add local name_lookup

--- arches_controlled_lists/media/js/viewmodels/reference-select.js
+++ arches_controlled_lists/media/js/viewmodels/reference-select.js
@@ -3,9 +3,9 @@ import ko from 'knockout';
 import koMapping from 'knockout-mapping';
 import arches from 'arches';
 import WidgetViewModel from 'viewmodels/widget';
-import NAME_LOOKUP from 'viewmodels/concept-select';
 
 export default function(params) {
+    NAME_LOOKUP = {};
     var self = this;
 
     params.configKeys = ['placeholder', 'defaultValue'];

bf488c23b911f970bc2ce22bd11e98286242b68b
Rob Gaston <rgaston@fargeo.com>
2025-05-22 16:17:26 -0700

    adds tests for get_child_uris

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from arches_controlled_lists.models import ListItem
+from arches_controlled_lists.models import List, ListItem
 
 # these tests can be run from the command line via
 # python manage.py test tests.test_models --settings="tests.test_settings"
@@ -17,3 +17,33 @@ class ListItemTests(TestCase):
 
         item.full_clean(exclude={"list"})
         self.assertIsNotNone(item.uri)
+
+
+class ListItemGetChildUrisTests(TestCase):
+    def setUp(self):
+        self.list = List.objects.create(name="Test List")
+        self.parent_item = ListItem.objects.create(
+            list=self.list, sortorder=0, uri="http://example.com/parent"
+        )
+        self.child_item_1 = ListItem.objects.create(
+            list=self.list,
+            parent=self.parent_item,
+            sortorder=1,
+            uri="http://example.com/child1",
+        )
+        self.child_item_2 = ListItem.objects.create(
+            list=self.list,
+            parent=self.parent_item,
+            sortorder=2,
+            uri="http://example.com/child2",
+        )
+
+    def test_get_child_uris_includes_parent_and_children(self):
+        uris = self.parent_item.get_child_uris()
+        self.assertIn(self.parent_item.uri, uris)
+        self.assertIn(self.child_item_1.uri, uris)
+        self.assertIn(self.child_item_2.uri, uris)
+
+    def test_get_child_uris_empty_for_item_without_children(self):
+        uris = self.child_item_1.get_child_uris()
+        self.assertEqual(uris, [self.child_item_1.uri])

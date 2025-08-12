963da5cfa833eb21e4a8f9556cbbcec5de3c5d74
Johnathan Clementi <johnathan.clementi1@gmail.com>
2025-08-12 19:17:02 -0400

    Add styling for import UI

--- arches_controlled_lists/src/arches_controlled_lists/components/misc/ImportList.vue
+++ arches_controlled_lists/src/arches_controlled_lists/components/misc/ImportList.vue
@@ -22,16 +22,33 @@ const toast = useToast();
 
 const emit = defineEmits<{
     (e: "imported"): void;
+    (e: "cancel"): void;
 }>();
 
 const formRef = ref();
 const file = ref<File | null>(null);
-const overwriteOption = ref("ignore");
+const overwriteOption = ref();
 
 const overwriteOptions = ref([
-    { label: $gettext("Ignore"), value: "ignore" },
-    { label: $gettext("Duplicate"), value: "duplicate" },
-    { label: $gettext("Overwrite"), value: "overwrite" },
+    {
+        label: $gettext("Ignore"),
+        value: "ignore",
+        tooltip: $gettext("Do nothing if the list or list item already exist"),
+    },
+    {
+        label: $gettext("Duplicate"),
+        value: "duplicate",
+        tooltip: $gettext(
+            "Create a new list or list item if it already exists",
+        ),
+    },
+    {
+        label: $gettext("Overwrite"),
+        value: "overwrite",
+        tooltip: $gettext(
+            "If a list already exists, replace it with the new one",
+        ),
+    },
 ]);
 
 function updateFileValue(event: { files: File[] }) {
@@ -55,7 +72,7 @@ async function submit() {
             toast.add({
                 severity: ERROR,
                 life: DEFAULT_ERROR_TOAST_LIFE,
-                summary: $gettext("Unable to fetch lists"),
+                summary: $gettext("Unable to import SKOS file"),
                 detail: error.message,
             });
         });
@@ -84,6 +101,7 @@ function overwriteResolver(formFieldVal: FormFieldResolverOptions) {
 <template>
     <Form
         ref="formRef"
+        class="form"
         @submit="submit"
     >
         <label for="fileUpload">SKOS File</label>
@@ -91,6 +109,7 @@ function overwriteResolver(formFieldVal: FormFieldResolverOptions) {
             v-slot="$field"
             name="fileUpload"
             :resolver="fileResolver"
+            class="form-fields"
         >
             <InputFile
                 v-model="file"
@@ -116,17 +135,30 @@ function overwriteResolver(formFieldVal: FormFieldResolverOptions) {
             v-slot="$field"
             name="overwrite-options"
             :resolver="overwriteResolver"
+            class="form-fields"
         >
             <span
                 v-for="option in overwriteOptions"
                 :key="option.value"
+                v-tooltip.bottom="{
+                    value: option.tooltip,
+                    showDelay: 1000,
+                    hideDelay: 300,
+                }"
+                class="radio-button-and-label"
             >
                 <RadioButton
                     v-model="overwriteOption"
                     :input-id="option.value"
                     :value="option.value"
+                    :initial-value="option.value"
+                    :aria-label="option.tooltip"
                 />
-                <label :for="option.value">{{ option.label }}</label>
+                <label
+                    :for="option.value"
+                    class="radio-label"
+                    >{{ option.label }}</label
+                >
             </span>
             <Message
                 v-for="error in $field.errors"
@@ -137,9 +169,42 @@ function overwriteResolver(formFieldVal: FormFieldResolverOptions) {
                 {{ error.message }}
             </Message>
         </FormField>
-        <Button
-            label="Submit"
-            type="submit"
-        />
+        <div class="p-dialog-footer">
+            <Button
+                label="Cancel"
+                type="button"
+                @click="$emit('cancel')"
+            />
+            <Button
+                label="Upload File"
+                type="submit"
+            />
+        </div>
     </Form>
 </template>
+<style scoped>
+.form {
+    margin-top: 1rem;
+}
+.p-dialog-footer {
+    padding: 0;
+    margin-top: 1rem;
+}
+.p-fileupload-basic {
+    justify-content: flex-start;
+}
+.form-fields {
+    padding-inline-start: 1rem;
+
+    label {
+        margin-bottom: 0;
+    }
+}
+.radio-button-and-label {
+    margin-right: 1.5rem;
+    margin-bottom: 0.5rem;
+}
+.radio-label {
+    margin-inline-start: 0.5rem;
+}
+</style>
